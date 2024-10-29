ce4df8d2f90e4d329012aa9a8960bf9557131e47
Zhuang Linsheng <zhuanglinsheng@outlook.com>
2024-10-30 01:34:17 +0800

    update-cmp

--- cmp/build.sh
+++ cmp/build.sh
@@ -1,4 +1,4 @@
 g++ -std=c++11 -O2 ./cmp_avltree.cpp ../src/tds_avltree.c  -o cmp_avltree.exe
-g++ -std=c++11 -O2 -flto ./cmp_avltree.cpp -ltds  -o test_avltree_dy.exe
-g++ -std=c++11 -O2 -flto ./cmp_avltree.cpp /usr/local/lib/libtds_static.a  -o test_avltree_st.exe
+g++ -std=c++11 -O2 -flto ./cmp_avltree.cpp -ltds  -o cmp_avltree_dy.exe
+g++ -std=c++11 -O2 -flto ./cmp_avltree.cpp /usr/local/lib/libtds_static.a  -o cmp_avltree_st.exe
 
